6ebe3a29ec03a1805eeaa423b61f3d74eb1fa2f6
FujiMac <165458514+FujiMac@users.noreply.github.com>
2024-09-21 12:33:03 +0900

    kadai_020

---
+++ kadai_020/event.js
@@ -0,0 +1,11 @@
+// btnというidを持つHTML要素を取得し、定数に代入する
+const btn = document.getElementById('btn');
+// textというidを持つHTML要素を取得し、定数に代入する
+const parentText = document.getElementById('text');
+
+// HTML要素がクリックされたときにイベント処理を実行する
+btn.addEventListener('click', () => {
+  // text要素に「ボタンをクリックしました」というテキストに変更する
+parentText.textContent = 'ボタンをクリックしました';
+
+});
